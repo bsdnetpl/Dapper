1591c8c367eb2ee9ec8be1e2b01684ec9475cd40
Adrian U <99587723+bsdnetpl@users.noreply.github.com>
2023-08-02 15:11:51 +0200

    Add project files.

---
+++ Dapper/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace Dapper.Models
+{
+    public class Person
+    {
+        public Guid Id { set; get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public DateTime dateTimeCreate { get; set; }
+    }
+
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(v => v.dateTimeCreate).NotNull();
+            RuleFor(v => v.Email).EmailAddress();
+
+        }
+    }
+}
