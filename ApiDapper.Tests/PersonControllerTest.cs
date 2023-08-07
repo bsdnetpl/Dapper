82554e1446f51edaec1e6a38bda4726b993cfeae
Adrian U <99587723+bsdnetpl@users.noreply.github.com>
2023-08-07 14:33:23 +0200

    add correctly tests

--- ApiDapper.Tests/PersonControllerTest.cs
+++ ApiDapper.Tests/PersonControllerTest.cs
@@ -8,15 +8,30 @@ namespace ApiDapper.Tests
 {
     public class PersonControllerTest
     {
-        [Fact]
-        public void TestGetPersonReturnPerson()
+        private static List<Person> GetFakePersonList()
+        {
+            return new List<Person>()
+           {  
+         new Person
+            {
+                Id = Guid.NewGuid(),
+                dateTimeCreate = DateTime.Now,
+                Email = "email@gmail.com",
+                FirstName = "xxx",
+                LastName = "xxx",
+                Password = "123123"
+            } 
+            };
+        }
 
+        [Fact]
+        public async Task TestGetPersonReturnPerson()
         {
             var mocGetPerson = new Mock<IPersonServices>();
-            mocGetPerson.Setup(m => m.GetPerson().Result).Returns(Task.FromResult<IEnumerable<Person>>) ;
-            IPersonServices personServices = mocGetPerson.Object;
+            mocGetPerson.Setup(m => m.GetPerson().Result).Returns(PersonControllerTest.GetFakePersonList);
+            var result = mocGetPerson.Object.GetPerson();
 
-            Assert.NotNull(personServices);
+            Assert.NotNull(result);
         }
     }
 }
